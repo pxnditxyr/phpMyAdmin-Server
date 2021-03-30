289c1983ec2a7b4c24aabd9cd5b4d178b69a1186
Pxndxs YR <pxnditxyr@gmail.com>
2021-03-30 01:50:16 -0400

    Stable Version

---
+++ alien.php
@@ -0,0 +1,19 @@
+
+<link rel="stylesheet" href="css/alien.css">
+<div class="ufo">
+	<div class="monster" style="color: #7cb342">
+		<div class="body">
+			<div class="ear"></div>
+			<div class="ear"></div>
+			<div class="vampi-mouth">
+				<div class="vampi-tooth"></div>
+			</div>
+		</div>
+		<div class="eye-lid">
+			<div class="eyes">
+				<div class="eye"></div>
+			</div>
+		</div>
+	</div>
+</div>
+<script src="./js/alien.js" ></script>
---
+++ js/alien.js
@@ -0,0 +1,9 @@
+let alien = document.querySelector(".alien");
+alien.addEventListener("mousemove", (e) => {
+	let eyes = document.querySelector(".eyes");
+	let mouseX = eyes.getBoundingClientRect().left;
+	let mouseY = eyes.getBoundingClientRect().top;
+	let radianDegrees = Math.atan2(e.pageX - mouseX, e.pageY - mouseY);
+	let rotationDegrees = radianDegrees * (180 / Math.PI) * -1 + 180;
+	eyes.style.transform = `rotate(${rotationDegrees}deg)`;
+});
